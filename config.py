9225f35c100fc95882a1543e8fb0a90e78080936
James Mwangi <james_mwangi@aol.com>
2019-02-19 14:11:45 +0300

    add configurations file

---
+++ config.py
@@ -0,0 +1,42 @@
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+
+    API_BASE_URL = 'http://quotes.stormconsultancy.co.uk/random.json'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://jamesmwangi:tj193345@localhost/blog'
+    SECRET_KEY = 'tj193345'
+    UPLOADED_PHOTOS_DEST ='app/static/photos'
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = "lilbratha@gmail.com"
+    MAIL_PASSWORD = "smallbrother1234."
+
+class ProdConfig(Config):
+    '''
+    Production  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+    pass
+    #SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+
+class DevConfig(Config):
+    '''
+    Development  configuration child class
+
+    Args:
+        Config: The parent configuration class with General configuration settings
+    '''
+
+    DEBUG = True
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig
+}
